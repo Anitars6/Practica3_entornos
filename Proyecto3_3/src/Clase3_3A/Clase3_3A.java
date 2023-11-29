18bf202ced1f8a7c127a989bc2bf4bcb796c7d67
Anitars6 <arodriguez4728@ceuandalucia.es>
2023-11-29 11:06:51 +0100

    Añado nueva clase que contiene un syso con nombres de mascotas.

---
+++ Proyecto3_3/src/Clase3_3A/Clase3_3A.java
@@ -0,0 +1,9 @@
+package Clase3_3A;
+
+public class Clase3_3A {
+
+	public static void main(String[] args) {
+		System.out.println("Mis mascotas se llaman Bombón, Pavarotti, Curra, Lola, Luna, Tara, Oreo y William");
+	}
+
+}
